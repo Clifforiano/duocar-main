fc4d5fbccb04f8722a7c6f3796555ae48a58aa0e
Clifforiano <cli.ramirez@duocuc.cl>
2024-10-23 14:03:15 -0300

    a
    a

---
+++ src/app/models/viaje.model.ts
@@ -0,0 +1,18 @@
+export interface Viaje {
+    id: string;
+    id_conductor: string;
+    id_pasajero: string;
+    id_vehiculo: string;
+    fecha: string; // Si es una fecha, puedes usar 'Date' en lugar de 'string'
+    precio: number; // Debe ser number si vas a hacer cálculos
+    // Ubicación inicial
+    latitudInicial: number; // Latitud debe ser de tipo number
+    longitudInicial: number; // Longitud debe ser de tipo number
+    horaInicio: Date; // Hora en formato de fecha
+    // Ubicación final
+    latitudFinal: number; // Latitud debe ser de tipo number
+    longitudFinal: number; // Longitud debe ser de tipo number
+    horaFinal: Date; // Hora en formato de fecha
+
+    estado: string;
+}
